cc276e1291c3032f7d4f91ed67a43a0272a87c4d
ellynoize <na1ton.trik@gmail.com>
2026-07-14 09:53:19 +0300

    bump

--- intaro.retailcrm/install/version.php
+++ intaro.retailcrm/install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = [
-    'VERSION'      => '6.8.4',
-    'VERSION_DATE' => '2026-07-01 12:00:00'
+    'VERSION'      => '6.8.5',
+    'VERSION_DATE' => '2026-07-14 12:00:00'
 ];
